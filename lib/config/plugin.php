936323a95b7f5fd5e9905094489fc75c30094763
kl <kl@10.66.3.1>
2014-09-08 10:06:24 +0600

    -упрощение процесса установки плагина -закрытие ссылки от индексации

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'Предпросмотр товаров',
     'description' => 'Быстрый просмотр товаров в каталоге',
     'vendor' => '985310',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'img' => 'img/productpreview.png',
     'shop_settings' => true,
     'frontend' => true,
