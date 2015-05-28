507270b9bc2f7b3b01a71a3c3fa8acc17e04fc39
admin <admin@xstream>
2015-05-28 15:13:14 +0600

    добавлена поддержка локализации

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'Предпросмотр товаров',
     'description' => 'Быстрый просмотр товаров в каталоге',
     'vendor' => '985310',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'img' => 'img/productpreview.png',
     'shop_settings' => true,
     'frontend' => true,
